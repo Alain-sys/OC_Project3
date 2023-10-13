e03cbcf7addc9bfe1b043819f63efdd5d54fecb4
Alain Remi <Alain-sys@protonmail.com>
2023-10-13 16:59:44 +0200

    feat: filters on projects

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,18 +1,28 @@
 const gallery = document.querySelector('.gallery');
 const filterContainer = document.querySelector('.filter-container');
-// const filterBtn = document.querySelectorAll('.filter-btn');
 
-function projects(data, btnCat) {
-  console.log('function', data);
-  data.map((item, id) => {
-    console.log(item, id);
-    gallery.innerHTML += `
+let projectData; // Projects of the API/works here for uses in other functions
+
+function projects(btnId) {
+  const projectFiltered = projectData.reduce((result, item) => {
+    if (item.category.id === btnId) {
+      result += `
         <figure>
             <img src="${item.imageUrl}" alt="${item.title}">
             <figcaption>${item.title}</figcaption>
         </figure>
-        `;
-  });
+      `;
+    } else if (!btnId) {
+      result += `
+        <figure>
+            <img src="${item.imageUrl}" alt="${item.title}">
+            <figcaption>${item.title}</figcaption>
+        </figure>
+      `;
+    }
+    return result;
+  }, '');
+  gallery.innerHTML = projectFiltered;
 }
 
 fetch('http://localhost:5678/api/works')
@@ -22,13 +32,13 @@ fetch('http://localhost:5678/api/works')
     }
     return response.json();
   })
-  .then((data) => {
-    projects(data);
+  .then((responseData) => {
+    projectData = responseData;
+    projects();
   })
   .catch((error) => {
     console.error("Une erreur s'est produite :", error);
   });
-
 fetch('http://localhost:5678/api/categories')
   .then((response) => {
     if (!response.ok) {
@@ -36,33 +46,26 @@ fetch('http://localhost:5678/api/categories')
     }
     return response.json();
   })
-  .then((data) => {
-    console.log(data);
-    data.map((item, id) => {
-      console.log('btn', item, id);
+  .then((responseData) => {
+    responseData.map((item) => {
       filterContainer.innerHTML += `
         <button class="filter-btn" type="button" data-id="${item.id}">${item.name}</button>
-        `;
+      `;
+    });
+
+    filterBtn = document.querySelectorAll('.filter-btn');
+    filterBtn.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        console.log('btnlist', btn);
+        filterBtn.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+        const btnId = parseInt(e.target.dataset.id, 10);
+        projects(btnId);
+      });
     });
   })
   .catch((error) => {
     console.error("Une erreur s'est produite :", error);
   });
-
-// filterBtn.forEach((btn) => {
-//   console.log('test', btn);
-
-//   btn.addEventListener('click', (e) => {
-//     console.log(e.target);
-//   });
-// });
-
-filterContainer.addEventListener('click', (e) => {
-  if (e.target.classList.contains('filter-btn')) {
-    console.log(e.target);
-    const btnId = e.target.dataset.id;
-    if (btnId === '1') {
-      console.log('hello');
-    }
-  }
-});
